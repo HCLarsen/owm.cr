44ac0f3ee9abf74285576eee48da074411a5f556
Chris Larsen <clarsenipod@gmail.com>
2017-10-16 17:56:40 -0400

    Refactor into single getCurrentWeather method

--- client.cr
+++ client.cr
@@ -5,23 +5,27 @@ require "./current_weather"
 
 # A client for interfacing with the Open Weather Map API.
 class OpenWeatherMap::Client
+  @@base_address = "http://api.openweathermap.org/data/2.5/"
   def initialize(key : String)
     @key = key
   end
 
-  # Requests current weather information for a single city by city ID and returns it as
-  # an instance of OpenWeatherMap::CurrentWeather.
-  def getCurrentWeatherFor(cityID : Int32)
-    response = HTTP::Client.get "http://api.openweathermap.org/data/2.5/weather?APPID=#{@key}&id=#{cityID}"
-    # Need code to check the response type before parsing.
+  # Requests current weather information for a single city by passing in required
+  # parameters as a hash.
+  # The Hash must have one of the following keys:
+  # q : Querying by the city name.
+  # id : Querying by the city ID.
+  # lat & lon : Querying by latitudinal and longitudinal values.
+  # zip : For American addresses, querying by the zip code.
+  def getCurrentWeatherFor(params : Hash)
+    params["APPID"] = @key
+    address = @@base_address + "/weather?" + HTTP::Params.encode(params)
+    response = HTTP::Client.get address
     value = JSON.parse(response.body)
-    CurrentWeather.new(value)
-  end
-  # Requests current weather information for a single city by name and returns it as
-  # an instance of OpenWeatherMap::CurrentWeather.
-  def getCurrentWeatherFor(city : String)
-    response = HTTP::Client.get "http://api.openweathermap.org/data/2.5/weather?APPID=#{@key}&q=#{URI.escape(city)}"
-    value = JSON.parse(response.body)
-    CurrentWeather.new(value)
+    if value["cod"].as_i >= 200 && value["cod"].as_i <= 299
+      CurrentWeather.new(value)
+    else
+      value["message"].as_s
+    end
   end
 end
--- openWeatherMap.cr
+++ openWeatherMap.cr
@@ -8,12 +8,29 @@ require "./client"
 module HTTP
 end
 
-key = "83658a490b36698e09e779d265859910"
+key = ENV["OWM"]
 city = "Mississauga"
 cityID = 6075357
+lat = 43.5
+long = -79.5
+zip = 94040
+
+params = { "id" => "6075357" }
 
 weather = OpenWeatherMap::Client.new(key)
-currentWeather = weather.getCurrentWeatherFor(city)
-puts currentWeather.simpleOutput
-currentWeather = weather.getCurrentWeatherFor(cityID)
-puts currentWeather.simpleOutput
+currentWeather = weather.getCurrentWeatherFor(params)
+case currentWeather
+when OpenWeatherMap::CurrentWeather
+  puts "Hash: " + currentWeather.simpleOutput
+else
+  puts currentWeather
+end
+
+#currentWeather = weather.getCurrentWeatherFor(cityID)
+#puts "CityID: " + currentWeather.simpleOutput
+#currentWeather = weather.getCurrentWeatherFor(city)
+#puts "City Name: " + currentWeather.simpleOutput
+#currentWeather = weather.getCurrentWeatherAt(lat, long)
+#puts "Lat and Long: " + currentWeather.simpleOutput
+#currentWeather = weather.getCurrentWeatherAtZip(zip)
+#puts "Zip Code: " + currentWeather.simpleOutput
