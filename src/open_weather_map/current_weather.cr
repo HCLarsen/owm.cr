c2053a34ae83dfb1b614fc512b4fbf5f36f6def7
Chris Larsen <clarsenipod@gmail.com>
2017-12-25 01:51:58 -0500

    Add Weather class tests

--- src/open_weather_map/current_weather.cr
+++ src/open_weather_map/current_weather.cr
@@ -2,7 +2,11 @@ require "./weather"
 
 # Contains all the information on the current weather status for any city.
 class OpenWeatherMap::CurrentWeather < OpenWeatherMap::Weather
-  #getter time : Time
+  #JSON.mapping(
+  #  name: { type: String, setter: false },
+  #  id: { type: Int32, setter: false },
+  #  time: { type: Time, setter: false },
+  #)
   getter name : String
   getter id : Int32
 
@@ -21,16 +25,16 @@ class OpenWeatherMap::CurrentWeather < OpenWeatherMap::Weather
   end
 
   # Outputs weather in a human readable format.
-  def simple_output
-    output = "Temperature in #{@name} at #{@time} is #{@temp} degrees, with #{@weather_description}. "
+  #def simple_output
+  #  output = "Temperature in #{@name} at #{@time} is #{@temp} degrees, with #{@weather_description}. "
+#
+#    if @wind_speed > 5 && @temp < 10
+#      windChill = (13.12 + 0.6215 * @temp - 11.37 * @wind_speed ** 0.16 + 0.3965 * @temp * @wind_speed ** 0.16).round(1)
+#      output += "Wind Speed is #{(@wind_speed * 3.6).round(1)}k/h, with a windchill of #{windChill} degrees."
+#    else
+#      output += "Wind Speed is #{(@wind_speed * 3.6).round(1)}km/h."
+#    end
 
-    if @wind_speed > 5 && @temp < 10
-      windChill = (13.12 + 0.6215 * @temp - 11.37 * @wind_speed ** 0.16 + 0.3965 * @temp * @wind_speed ** 0.16).round(1)
-      output += "Wind Speed is #{(@wind_speed * 3.6).round(1)}k/h, with a windchill of #{windChill} degrees."
-    else
-      output += "Wind Speed is #{(@wind_speed * 3.6).round(1)}km/h."
-    end
-
-    output
-  end
+#    output
+#  end
 end
--- test/weather_test.cr
+++ test/weather_test.cr
@@ -7,9 +7,7 @@ require "/../src/open_weather_map/current_weather"
 class CurrentWeatherTest < Minitest::Test
   def test_parses_json
     value = JSON.parse(%({"coord":{"lon":-79.5,"lat":43.5},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":277.44,"pressure":1006,"humidity":63,"temp_min":276.15,"temp_max":279.15},"visibility":14484,"wind":{"speed":8.2,"deg":260,"gust":13.4},"clouds":{"all":1},"dt":1513724400,"sys":{"type":1,"id":3721,"message":0.2431,"country":"CA","sunrise":1513687641,"sunset":1513719852},"id":6111708,"name":"Port Credit","cod":200}))
-    currentWeather = OpenWeatherMap::CurrentWeather.new(value)
-    assert_equal currentWeather.name, "Port Credit"
-    assert_equal currentWeather.id, 6111708
+    currentWeather = OpenWeatherMap::Weather.new(value)
     assert_equal currentWeather.time, Time.epoch(1513724400).to_local
     assert_equal currentWeather.temp, 4.3
     assert_equal currentWeather.temp_min, 3
@@ -24,20 +22,4 @@ class CurrentWeatherTest < Minitest::Test
     assert_equal currentWeather.snow, 0
     assert_equal currentWeather.rain, 0
   end
-
-#  def test_fetches_weather_from_coordinates
-#    key = ENV["OWM"]
-#    client = OpenWeatherMap::Client.new(key)
-#    params = { "lat" => 43.5, "lon" => -79.5}
-#    currentWeather = client.current_weather_for_city(params)
-#    assert_equal currentWeather.class, OpenWeatherMap::CurrentWeather
-#  end
-#
-#  def test_doesnt_fetch_without_params
-#    key = ENV["OWM"]
-#    client = OpenWeatherMap::Client.new(key)
-#    params = {} of String => String
-#    currentWeather = client.current_weather_for_city(params)
-#    assert_equal currentWeather.class, String
-#  end
 end
